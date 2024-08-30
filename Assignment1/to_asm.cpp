5c3c740496ce17b4fceada9407ee9a45d4f672da
sankyaku <70568399+gafnaa@users.noreply.github.com>
2024-08-30 10:02:07 +0700

    Add files via upload

---
+++ Assignment1/to_asm.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int16_t num1 = 5;
+    int16_t num2 = 10;
+    int16_t result = 0;
+
+    result = num1 * num2;
+
+    // Exit the program
+    return 0;
+}
